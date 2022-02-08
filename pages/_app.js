60b67092863c443717b36987de26ac6677a635ed
Osama Husam <osamahusam848@gmail.com>
2022-02-08 13:29:05 +0200

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,9 @@
-import '../styles/globals.css'
-
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import Layout from '../components/Layouts/Layout '
+import '../styles/main.scss'
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  )
 }
-
-export default MyApp
